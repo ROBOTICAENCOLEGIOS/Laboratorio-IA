7d4a6eb83a97a2822817c1e8ca95fd3bf6e61c86
ROBOTICAENCOLEGIOS <ernesto@belgranouno.com.ar>
2026-05-04 19:36:32 -0300

    Actualizada URL a ?v=333 para forzar actualización drástica de caché

--- js/embed.js
+++ js/embed.js
@@ -38912,7 +38912,7 @@ const vozIconURL = 'https://raw.githubusercontent.com/ROBOTICAENCOLEGIOS/ia-robo
   incompatibleWithScratch: true,
   unsandboxed: true,
   useUnsandboxed: true,
-  extensionURL: 'https://cdn.jsdelivr.net/gh/ROBOTICAENCOLEGIOS/ia-robotica@main/extensionesrec/iamanos.js?v=2',
+  extensionURL: 'https://cdn.jsdelivr.net/gh/ROBOTICAENCOLEGIOS/ia-robotica@main/extensionesrec/iamanos.js?v=333',
   tags: ['@roboticaencolegios']
 }, {
   name: 'Señales de Tránsito',
